c460e90ac89eb3e768368fba6a48f194049fe2cc
bidyut das <bidyutdass@gmail.com>
2014-07-20 00:43:34 +0530

    ark added.

---
+++ ark/Berksfile
@@ -0,0 +1,12 @@
+source "http://api.berkshelf.com"
+
+metadata
+
+group :integration do
+  cookbook 'apt'
+  cookbook 'minitest-handler'
+  cookbook 'build-essential'
+end
+
+
+cookbook "ark_spec", path: "spec/fixtures/cookbooks/ark_spec"
---
+++ ark/spec/recipes/windows/default_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe_recipe "ark::default" do
+
+  def node_attributes
+    { platform: "windows", version: "2008R2" }
+  end
+
+  let(:expected_packages) do
+    %w( libtool autoconf unzip rsync make gcc autogen xz-lzma-compat )
+  end
+
+  it "does not installs packages" do
+    expected_packages.each do |package|
+      expect(chef_run).not_to install_package(package)
+    end
+  end
+
+  it "does include the 7-zip recipe" do
+    expect(chef_run).to include_recipe("7-zip")
+  end
+
+  context "sets default attributes" do
+
+    it "tar binary" do
+      expect(default_cookbook_attribute("tar")).to eq %("\\7-zip\\7z.exe")
+    end
+
+  end
+
+end
